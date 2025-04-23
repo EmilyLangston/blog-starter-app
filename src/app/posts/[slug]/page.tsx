bd0424ba2f8d860426970b21bb92cbdd5b42ca2e
EmilyLangston <emilylangston129@gmail.com>
2025-04-23 17:17:34 -0100

    fixed posts/[slug]/page.tsx use of params

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,26 +1,17 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { getAllPosts, getPostBySlug } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
+import { getPostBySlug } from "@/lib/api";
 import markdownToHtml from "@/lib/markdownToHtml";
 import Container from "@/app/_components/container";
 import { PostBody } from "@/app/_components/post-body";
 import { PostHeader } from "@/app/_components/post-header";
 import { Post } from "@/interfaces/post";
-import Image from "next/image";
 
-type Params = {
-  params: {
-    slug: string;
-  };
-};
+export default async function PostPage(props: { params: { slug: string } }) {
+  const { slug } = await props.params; // 👈 await before accessing `slug`
 
-export default async function Post({ params }: Params) {
-  const post: Post | undefined = await getPostBySlug(params.slug);
-
-  if (!post) {
-    return notFound();
-  }
+  const post: Post | undefined = await getPostBySlug(slug);
+  if (!post) return notFound();
 
   const content = await markdownToHtml(post.content || "");
 
@@ -34,35 +25,9 @@ export default async function Post({ params }: Params) {
             date={post.date}
             author={post.author || { name: "", picture: "" }}
           />
-          <PostBody content={content} ogImage={post.ogImage.url} />
+          <PostBody content={content} ogImage={post.ogImage?.url || ""} />
         </article>
       </Container>
     </main>
   );
-}
-
-export async function generateMetadata({ params }: Params): Promise<Metadata> {
-  const post: Post | undefined = await getPostBySlug(params.slug);
-
-  if (!post) {
-    return notFound();
-  }
-
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
-
-  return {
-    title,
-    openGraph: {
-      title,
-      images: [post.ogImage?.url || ""],
-    },
-  };
-}
-
-export async function generateStaticParams() {
-  const posts: Post[] = await getAllPosts();
-
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
 }
